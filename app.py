4ee82fe65eb13906a7b08816d784d28584036edd
Sukma Ramadhan Asri <59074406+onedayxzn@users.noreply.github.com>
2022-06-03 01:25:16 +0700

    adding features
    new progress 3

--- app.py
+++ app.py
@@ -31,95 +31,83 @@ def index():
 
 
 def cal_accum_avg(frame, accumulated_weight):
-
     global background
-
     if background is None:
         background = frame.copy().astype("float")
-        return None
-
+        return
     cv2.accumulateWeighted(frame, background, accumulated_weight)
 
 
 def segment_hand(frame, threshold=25):
     global background
-
     diff = cv2.absdiff(background.astype("uint8"), frame)
-
-    _, thresholded = cv2.threshold(diff, threshold, 255,
-                                   cv2.THRESH_BINARY)
-
-    # Fetching contours in the frame (These contours can be of handor any other object in foreground) …
+    _, thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
 
     contours, hierarchy = cv2.findContours(
         thresholded.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # If length of contours list = 0, means we didn't get anycontours...
     if len(contours) == 0:
         return None
     else:
-        # The largest external contour should be the hand
         hand_segment_max_cont = max(contours, key=cv2.contourArea)
-
-        # Returning the hand segment(max contour) and thethresholded image of hand...
         return (thresholded, hand_segment_max_cont)
 
 
 def sign_frames():
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(1)
     num_frames = 0
     while True:
-        success, frame = camera.read()
-        frame = cv2.flip(frame, 1)
-        if frame is None:
-            break
-        frame_copy = frame.copy()
 
-        # ROI
-        roi = frame[ROI_top:ROI_bottom, ROI_right:ROI_left]
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (9, 9), 0)
+        ret, frame = camera.read()  # read the camera frame
 
-        if num_frames < 70:
-            cal_accum_avg(gray, accumulated_weight)
+        while True:
+            ret, frame = camera.read()
+            frame = cv2.flip(frame, 1)
 
-            cv2.putText(frame_copy, "FETCHING BACKGROUND...PLEASE WAIT",
-                        (80, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+            # ROI
+            roi = frame[ROI_top:ROI_bottom, ROI_right:ROI_left]
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (9, 9), 0)
 
-        else:
-            hand = segment_hand(gray)
-            if hand is not None:
-                thresholded, hand_segment = hand
-                # Drawing contours around hand segment
-                cv2.drawContours(frame_copy, [hand_segment + (ROI_right,
-                                                              ROI_top)], -1, (255, 0, 0), 1)
+            if num_frames < 70:
+                cal_accum_avg(gray, accumulated_weight)
 
-                thresholded = cv2.resize(thresholded, (160, 160))
-                thresholded = cv2.cvtColor(
-                    thresholded, cv2.COLOR_BGR2GRAY)
-                thresholded = np.reshape(
-                    thresholded, ((1, thresholded.shape[0], thresholded.shape[1], 3)))
-                pred = model.predict(thresholded)
-                cv2.putText(frame_copy, word_dict1[np.argmax(pred)],
-                            (170, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(frame, "",
+                            (80, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+            else:
+                hand = segment_hand(gray)
+                if hand is not None:
+                    thresholded, hand_segment = hand
+                    # Drawing contours around hand segment
+                    cv2.drawContours(frame, [hand_segment + (ROI_right,
+                                                             ROI_top)], -1, (255, 0, 0), 1)
+
+                    thresholded = cv2.resize(thresholded, (160, 160))
+                    thresholded = cv2.cvtColor(
+                        thresholded, cv2.COLOR_BGR2GRAY)
+                    thresholded = np.reshape(thresholded,
+                                             (1, thresholded.shape[0], thresholded.shape[1], 3))
+                    pred = model.predict(thresholded)
+                    cv2.putText(frame, word_dict1[np.argmax(pred)],
+                                (170, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             # draw roi
-        cv2.rectangle(frame_copy, (ROI_left, ROI_top),
-                      (ROI_right, ROI_bottom), (255, 128, 0), 3)
-        num_frames += 1
+            cv2.rectangle(frame, (ROI_left, ROI_top),
+                          (ROI_right, ROI_bottom), (255, 128, 0), 3)
+            num_frames += 1
 
-        # display frame
-        cv2.putText(frame_copy, "DataFlair hand sign recognition_ _ _",
-                    (50, 50), cv2.FONT_ITALIC, 0.5, (51, 255, 51), 1)
+            # display frame
+            cv2.putText(frame, "sign recognition_ _ _",
+                        (50, 50), cv2.FONT_ITALIC, 0.5, (51, 255, 51), 1)
 
-        camera.release()
-        cv2.destroyAllWindows()
-        try:
-            ret, buffer = cv2.imencode('.jpg', frame_copy)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        except Exception as e:
-            pass
+            camera.release()
+            cv2.destroyAllWindows()
+            try:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
 
 
 @app.route('/video_feed')
